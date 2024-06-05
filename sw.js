e0713ed7fae3c051b3f0f179e48d3db49435360a
Alex Reis <129780074+Alexreys84@users.noreply.github.com>
2024-06-05 19:55:34 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Nome do cache (Controle de versão)
+const cachePWA = 'cache-v1'
+// Arquivos a serem armazenados em cache
+// todos os arquivos deve ser adicionados ao vetor(exeto o manifesto)
+const urlsToCache = [
+  '/',
+  '/index.html', 
+  '/style.css',
+  '/app.js',
+  '/sw.js', 
+  '/hal192.png',
+  '/hal512.png'
+]
+
+// Instalando o Service Worker e armazenando os arquivos no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cachePWA)
+      .then((cache) => {
+        return cache.addAll(urlsToCache)
+      })
+  )
+})
+
+// Interceptando as solicitações de rede e servindo arquivos do cache quando offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Se o arquivo está no cache, serve o arquivo do cache
+        if (response) {
+          return response
+        }
+        // Caso contrário, faz uma solicitação de rede
+        return fetch(event.request)
+      })
+  )
+})
