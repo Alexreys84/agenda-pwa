975bfed8a8f42253cc75ea2a0b7f11d4b5756586
Alex Reis <129780074+Alexreys84@users.noreply.github.com>
2024-06-07 22:31:03 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,60 +1,38 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installed');
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      // Adiciona arquivos ao cache durante a instalação
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js',
-        '/manifest.json',
-        '/icon.png',
-        '/list.png'
-      ]);
-    })
-  );
-});
+// Nome do cache (Controle de versão)
+const cachePWA = 'cache-v1'
+// Arquivos a serem armazenados em cache
+// todos os arquivos deve ser adicionados ao vetor(exeto o manifesto)
+const urlsToCache = [
+  '/',
+  '/index.html', 
+  '/style.css',
+  '/app.js',
+  '/sw.js', 
+  '/hal192.png',
+  '/hal512.png'
+]
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-<<<<<<< HEAD
-    caches.match(event.request).then((response) => {
-      // Retorna a resposta em cache ou faz uma nova solicitação
-      return response || fetch(event.request);
-    })
-  );
-});
-=======
+// Instalando o Service Worker e armazenando os arquivos no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(cachePWA)
       .then((cache) => {
-        return cache.match(event.request)
-          .then((response) => {
-            // Se o arquivo está no cache, serve o arquivo do cache
-            if (response) {
-              return response;
-            }
-
-            // Caso contrário, faz uma solicitação de rede
-            return fetch(event.request)
-              .then((networkResponse) => {
-                // Se a solicitação for bem-sucedida, armazena a resposta em cache
-                if (networkResponse.ok) {
-                  cache.put(event.request, networkResponse.clone());
-                }
-                return networkResponse;
-              })
-              .catch(() => {
-                // Se a solicitação falhar, retorna uma resposta padrão
-                // Ou uma página de erro personalizada
-                return new Response('Você está offline', {
-                  status: 503,
-                  statusText: 'Offline'
-                });
-              });
-          });
+        return cache.addAll(urlsToCache)
       })
-  );
-});
+  )
+})
 
->>>>>>> 62cd157381714193789feeaa9f1fc9ef6a43a1a8
+// Interceptando as solicitações de rede e servindo arquivos do cache quando offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Se o arquivo está no cache, serve o arquivo do cache
+        if (response) {
+          return response
+        }
+        // Caso contrário, faz uma solicitação de rede
+        return fetch(event.request)
+      })
+  )
+})
